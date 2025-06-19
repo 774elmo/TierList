8682418acc9c728dc5b55048907bb5d4bcc6b511
774elmo <774elmo@gmail.com>
2025-06-20 00:04:08 +0800

    Update API link to https://api.lifestealpvp.xyz/api/v1/data

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://lifestealpvp.xyz',
+        target: 'https://api.lifestealpvp.xyz',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
       },
